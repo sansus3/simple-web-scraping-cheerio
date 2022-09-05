9909188e8431c4225d920fa0ca554d5c76794287
webferrol <xurxo@webferrol.com>
2022-09-05 22:54:55 +0200

    Añadio cheerio

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Web scratching
-app para inicializarse en el _web scratching_ utilizando [cheerio](https://cheerio.js.org/) como base
+app para inicializarse en el _web scratching_ utilizando [cheerio](https://cheerio.js.org/) como base.
+Seguimos el siguiente vídeo en [twitch](https://www.twitch.tv/videos/1579320719) realizado por _Ignacio Gutiérrez_
 
 ## Paquetes instalados
 1. express
 2. nodemon
 3. axios
 4. [cheerio](https://cheerio.js.org/)
+
+## express
+Para configurar de forma dinámica el puerto
+
+```JavaScript
+const PORT = process.env.PORT || 5000;
+```
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 import express from "express"; //podemos importar express puesto que pusiomo type: "module" en package.json
+import axios from "axios";
+import * as cheerio from 'cheerio';
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get('/',async (req,res)=>{
+
+    try {
+        const {data} = await axios.get('https://webferrol-app.web.app/about');
+        const $ = cheerio.load(data);
+        res.json(
+            {
+                "date": new Date().toLocaleDateString(),
+                "titulo":$('title').text()})
+    } catch (error) {
+        res.json(error)
+    }
+
+})
+
+
 app.listen(PORT,()=>console.log("Escuchando ❤"));

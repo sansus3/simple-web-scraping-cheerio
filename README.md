2d79ad46e60a1fcb5909b3d00e841c70438556cd
webferrol <xurxo@webferrol.com>
2022-09-05 23:55:39 +0200

    Añadido cors. Para ver información ver README.md

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Seguimos el siguiente vídeo en [twitch](https://www.twitch.tv/videos/1579320719
 
 ## Paquetes instalados
 1. express
+    * express
+    * [cors](https://expressjs.com/en/resources/middleware/cors.html)
 2. nodemon
 3. axios
 4. [cheerio](https://cheerio.js.org/)
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express"; //podemos importar express puesto que pusiomo type: "module" en package.json
+import cors from "cors";
 import axios from "axios";
 import * as cheerio from 'cheerio';
 const app = express();
+
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/',async (req,res)=>{
+app.get('/', cors(), async (req,res)=>{
 
     try {
         const {data} = await axios.get('https://webferrol-app.web.app/about');
